671d651ac9eb6bda584c904f49c89899b6e96752
Michael Limbird <m-limbird@onu.edu>
2015-03-24 23:18:54 -0400

    these will be entered

--- mikesApp.js
+++ mikesApp.js
@@ -28,15 +28,15 @@ if (Meteor.isClient) {
 
   // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
   Template.body.events({
-  "submit .new-task": function (event) {
+  "click #submitButton": function (event) {
     // This function is called when the new task form is submitted
 
-    var text = event.target.text.value;
+    var text = $("#submitInput").val();
 
     Meteor.call("addTask", text);
 
     // Clear form
-    event.target.text.value = "";
+    $("#submitInput").val("");
 
     // Prevent default form submit
     return false;
@@ -52,6 +52,52 @@ if (Meteor.isClient) {
   Template.task.helpers({
     isOwner: function () {
       return this.owner === Meteor.userId();
+    },
+    elapsedTime: function () {
+      var timeString = "";
+      var timeDesc = "";
+      currentTime = new Date();
+      elapsed = currentTime - this.createdAt; //time in milliseconds
+      if (elapsed >= 86400000) {
+        time = elapsed / 86400000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Day";
+        else
+          timeDesc = " Days";
+        timeString = time.toFixed(0) + timeDesc;
+      } else if (elapsed >= 3600000){
+        time = elapsed / 3600000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Hour";
+        else
+          timeDesc = " Hours";
+        timeString = time.toFixed(0) + timeDesc;
+      } else if (elapsed >= 60000){
+        time = elapsed / 60000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Minute";
+        else
+          timeDesc = " Minutes";
+        timeString = time.toFixed(0) + timeDesc;
+      } else if (elapsed >= 1000){
+        time = elapsed / 1000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Second";
+        else
+          timeDesc = " Seconds";
+        timeString = time.toFixed(0) + timeDesc;
+      }  
+      
+      return timeString;
+    },
+    textDisplay: function () {
+      var longString = "";
+      if (this.text.length > 50)
+        longString = this.text.substring(0,50) + "...";
+      else
+        longString = this.text;
+
+      return longString;
     }
   });
 
@@ -130,3 +176,9 @@ if (Meteor.isServer) {
     });
   });
 }
+
+Router.route('/', function () {
+  this.render('Home', {
+    data: function () { return Items.findOne({_id: this.params._id}); }
+  });
+});
