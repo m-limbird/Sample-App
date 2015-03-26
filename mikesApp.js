b614e38370fbc4b9e294a0f378bb99ae5b88698b
Michael Limbird <m-limbird@onu.edu>
2015-03-26 13:22:50 -0400

    Added new functionality, and cleaned up some files.

--- mikesApp.js
+++ mikesApp.js
@@ -6,7 +6,7 @@ if (Meteor.isClient) {
   Meteor.subscribe("tasks");
 
   // Replace the existing Template.body.helpers
-  Template.body.helpers({
+  Template.App.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")) {
         // If hide completed is checked, filter tasks
@@ -27,7 +27,7 @@ if (Meteor.isClient) {
 });
 
   // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
-  Template.body.events({
+  Template.App.events({
   "click #submitButton": function (event) {
     // This function is called when the new task form is submitted
 
@@ -41,13 +41,101 @@ if (Meteor.isClient) {
     // Prevent default form submit
     return false;
   },
-
   // Add to Template.body.events
   "change .hide-completed input": function (event) {
     Session.set("hideCompleted", event.target.checked);
   }
-  
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  Template.Post.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    },
+    elapsedTime: function () {
+      var timeString = "";
+      var timeDesc = "";
+      currentTime = new Date();
+      elapsed = currentTime - this.createdAt; //time in milliseconds
+      if (elapsed >= 86400000) {
+        time = elapsed / 86400000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Day";
+        else
+          timeDesc = " Days";
+        timeString = time.toFixed(0) + timeDesc;
+      } else if (elapsed >= 3600000){
+        time = elapsed / 3600000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Hour";
+        else
+          timeDesc = " Hours";
+        timeString = time.toFixed(0) + timeDesc;
+      } else if (elapsed >= 60000){
+        time = elapsed / 60000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Minute";
+        else
+          timeDesc = " Minutes";
+        timeString = time.toFixed(0) + timeDesc;
+      } else if (elapsed >= 1000){
+        time = elapsed / 1000;
+        if (time.toFixed(0) == 1)
+          timeDesc = " Second";
+        else
+          timeDesc = " Seconds";
+        timeString = time.toFixed(0) + timeDesc;
+      }  
+      
+      return timeString;
+    },
+    textDisplay: function () {
+      var longString = "";
+      if (this.text.length > 50)
+        longString = this.text.substring(0,50) + "...";
+      else
+        longString = this.text;
+
+      return longString;
+    }
+  });
+
+  // In the client code, below everything else
+  Template.Post.events({
+  "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Meteor.call("setChecked", this._id, ! this.checked);
+  },
+  "click .delete": function () {
+    Meteor.call("deleteTask", this._id);
+  },
+  "click .toggle-private": function () {
+    Meteor.call("setPrivate", this._id, ! this.private);
+  },
+  // "click #the-task": function () {
+  //   Router.go('Post', {theTask: this._id});
+  // }
+  });
+
+
+
+
+
+
+
+
+
 
   Template.task.helpers({
     isOwner: function () {
@@ -112,8 +200,12 @@ if (Meteor.isClient) {
   },
   "click .toggle-private": function () {
     Meteor.call("setPrivate", this._id, ! this.private);
-  }
+  },
+  // "click #the-task": function () {
+  //   Router.go('Post', {theTask: this._id});
+  // }
   });
+
 
   // At the bottom of the client code
   Accounts.ui.config({
@@ -177,8 +269,18 @@ if (Meteor.isServer) {
   });
 }
 
-Router.route('/', function () {
-  this.render('Home', {
-    data: function () { return Items.findOne({_id: this.params._id}); }
-  });
-});
+// Router.route('/', function () {
+//   this.render('Home', {
+//     data: function () { return Items.findOne({_id: this.params._id}); }
+//   });
+// });
+
+Router.map(function(){
+  this.route('App', {path: '/'});
+  this.route('/Post/:_id', {
+    name: 'Post',
+    data: function(){
+      return Tasks.findOne(this.params._id);
+    }
+  })
+})
